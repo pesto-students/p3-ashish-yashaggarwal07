39e07f1b81930fab95451544e57c672a55bdaae7
yashaggarwal <ysinghal73@gmail.com>
2022-07-10 16:59:10 +0530

    assignment 3 fibonaci series

--- Week-4/assignment3.js
+++ Week-4/assignment3.js
@@ -1,1 +1,31 @@
 // Assignment 3
+const Fib = (n) => ({
+    [Symbol.iterator]: function () {
+        let i = 1, prev = 0, next = 0;
+        return{
+            next: ()=> {
+                if(i++ <= n){
+                    [prev , next] = [next,((prev + next) || 1)];
+                    return {done: false , value: prev};
+                }
+                return {done: true, value: undefined};
+            }
+        }
+    }
+})
+
+console.log([...Fib(10)]);
+//[0,1,1,2,3,5,8,13,21,34]
+
+for( let num of Fib(7)){
+    console.log(num);
+}
+/** Output:
+ * 0
+ * 1
+ * 1
+ * 2
+ * 3
+ * 5
+ * 8
+ */
