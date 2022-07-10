fb6999490284d2e03fd2d33a27588b21facb5817
yashaggarwal <ysinghal73@gmail.com>
2022-07-10 16:41:02 +0530

    Assignment 2

--- Week-4/assignment2.js
+++ Week-4/assignment2.js
@@ -1,2 +1,20 @@
-// Assignment 2
+//Assignment 2
+var Person = function (){};
 
+Person.prototype.initialize = function (name,age){
+    this.name = name;
+    this.age = age;
+}
+ var Teacher = function(name,age){
+    Person.call(this,name,age)
+ }
+
+Teacher.prototype.teach = function(subject){
+    return `${this.name} is now teaching ${subject}`;
+}
+
+Object.setPrototypeOf(Teacher.prototype,Person.prototype);
+
+var man = new Teacher();
+man.initialize('Yash',25);
+console.log(man.teach('Inheritance')); // Yash is teaching Inheritance
